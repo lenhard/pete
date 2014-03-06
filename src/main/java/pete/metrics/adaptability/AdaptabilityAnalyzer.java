f02d948d9b7e9066329096dbe83dd79cf9ff82a8
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2014-03-06 15:55:20 +0100

    Extract several methods to utility class

--- src/main/java/pete/metrics/adaptability/AdaptabilityAnalyzer.java
+++ src/main/java/pete/metrics/adaptability/AdaptabilityAnalyzer.java
@@ -13,10 +13,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
@@ -29,9 +26,12 @@ public class AdaptabilityAnalyzer implements FileAnalyzer {
 
 	private ElementCounter elementCounter;
 
+	private BpmnInspector inspector;
+
 	public AdaptabilityAnalyzer() {
 		// Currently always do strict parsing
 		elementCounter = new ElementCounter();
+		inspector = new BpmnInspector();
 	}
 
 	@Override
@@ -55,10 +55,12 @@ public class AdaptabilityAnalyzer implements FileAnalyzer {
 
 		Document dom = getDom(entry.getFileName());
 
-		entry.addVariable("isCorrectNamespace", isCorrectNamespace(dom));
-		Node process = getProcess(dom);
-		entry.addVariable("isExecutable", isExecutable(process));
-		entry.addVariable("elements", getNumberOfChildren(process) + "");
+		entry.addVariable("isCorrectNamespace",
+				inspector.isCorrectNamespace(dom));
+		Node process = inspector.getProcess(dom);
+		entry.addVariable("isExecutable", inspector.isExecutable(process));
+		entry.addVariable("elements", inspector.getNumberOfChildren(process)
+				+ "");
 
 		elementCounter.addToCounts(process);
 	}
@@ -95,82 +97,6 @@ public class AdaptabilityAnalyzer implements FileAnalyzer {
 
 		}
 		return dom;
-	}
-
-	private Node getProcess(Document dom) throws AnalysisException {
-		NodeList nodes = getChildrenOfDefinitions(dom);
-		if (nodes == null) {
-			throw new AnalysisException(
-					"File cannot be analyzed: no definitions element or definitions element is empty");
-		}
-		for (int i = 0; i < nodes.getLength(); i++) {
-			Node node = nodes.item(i);
-			if ("process".equals(node.getLocalName())) {
-				return node;
-			}
-		}
-		throw new AnalysisException(
-				"File cannot be analyzed: no process element found");
-	}
-
-	private String isCorrectNamespace(Document dom) {
-		Element root = null;
-		try {
-			root = dom.getDocumentElement();
-		} catch (NullPointerException e) {
-			return "false";
-		}
-		if ("http://www.omg.org/spec/BPMN/20100524/MODEL".equals(root
-				.getNamespaceURI())) {
-			return "true";
-		} else {
-			return "false";
-		}
-	}
-
-	private String isExecutable(Node process) {
-		NamedNodeMap attributes = process.getAttributes();
-
-		for (int j = 0; j < attributes.getLength(); j++) {
-			Node attribute = attributes.item(j);
-			if ("isExecutable".equals(attribute.getLocalName())
-					&& "true".equals(attribute.getNodeValue())) {
-				return "true";
-			}
-		}
-
-		return "false";
-	}
-
-	private NodeList getChildrenOfDefinitions(Document dom) {
-		NodeList nodes = null;
-		try {
-			nodes = dom.getChildNodes();
-		} catch (NullPointerException e) {
-			return null;
-		}
-		for (int i = 0; i < nodes.getLength(); i++) {
-			Node node = nodes.item(i);
-			if ("definitions".equals(node.getLocalName())) {
-				return node.getChildNodes();
-			}
-		}
-		return null;
-	}
-
-	private int getNumberOfChildren(Node node) {
-		NodeList children = node.getChildNodes();
-		int result = 0;
-
-		if (children == null) {
-			// do noting, result is zero
-		} else {
-			result += children.getLength();
-			for (int i = 0; i < children.getLength(); i++) {
-				result += getNumberOfChildren(children.item(i));
-			}
-		}
-		return result;
 	}
 
 	@Override
---
+++ src/main/java/pete/metrics/adaptability/BpmnInspector.java
@@ -0,0 +1,90 @@
+package pete.metrics.adaptability;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import pete.executables.AnalysisException;
+
+class BpmnInspector {
+
+	private static final String BPMN_NAMESPACE = "http://www.omg.org/spec/BPMN/20100524/MODEL";
+
+	String isCorrectNamespace(Document dom) {
+		Element root = null;
+		try {
+			root = dom.getDocumentElement();
+		} catch (NullPointerException e) {
+			return "false";
+		}
+		if (BPMN_NAMESPACE.equals(root.getNamespaceURI())) {
+			return "true";
+		} else {
+			return "false";
+		}
+	}
+
+	String isExecutable(Node process) {
+		NamedNodeMap attributes = process.getAttributes();
+
+		for (int j = 0; j < attributes.getLength(); j++) {
+			Node attribute = attributes.item(j);
+			if ("isExecutable".equals(attribute.getLocalName())
+					&& "true".equals(attribute.getNodeValue())) {
+				return "true";
+			}
+		}
+
+		return "false";
+	}
+
+	int getNumberOfChildren(Node node) {
+		NodeList children = node.getChildNodes();
+		int result = 0;
+
+		if (children == null) {
+			// do noting, result is zero
+		} else {
+			result += children.getLength();
+			for (int i = 0; i < children.getLength(); i++) {
+				result += getNumberOfChildren(children.item(i));
+			}
+		}
+		return result;
+	}
+
+	Node getProcess(Document dom) throws AnalysisException {
+		NodeList nodes = getChildrenOfDefinitions(dom);
+		if (nodes == null) {
+			throw new AnalysisException(
+					"File cannot be analyzed: no definitions element or definitions element is empty");
+		}
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node node = nodes.item(i);
+			if ("process".equals(node.getLocalName())) {
+				return node;
+			}
+		}
+		throw new AnalysisException(
+				"File cannot be analyzed: no process element found");
+	}
+
+	private NodeList getChildrenOfDefinitions(Document dom) {
+		NodeList nodes = null;
+		try {
+			nodes = dom.getChildNodes();
+		} catch (NullPointerException e) {
+			return null;
+		}
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node node = nodes.item(i);
+			if ("definitions".equals(node.getLocalName())) {
+				return node.getChildNodes();
+			}
+		}
+		return null;
+	}
+
+}
