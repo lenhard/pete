88c343608e585191375d5f9a36621867b9f22cb7
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2014-04-08 11:48:48 +0200

    Add tests for parallel eventBasedGateway

--- src/main/java/pete/metrics/adaptability/elements/GatewayElements.java
+++ src/main/java/pete/metrics/adaptability/elements/GatewayElements.java
@@ -18,12 +18,22 @@ class GatewayElements {
 		buildParallelGateway();
 		buildComplexGateway();
 		buildEventBasedGateway();
+		buildParallelEventBasedGateway();
+	}
+
+	private void buildParallelEventBasedGateway() {
+		AdaptableElement eventBasedGateway = new AdaptableElement("parallelEventBasedGateway");
+		eventBasedGateway.setDocumentation("A parallel eventBasedGateway can be adapted to a solution where the events are processed through tasks before the gateway and set conditions to be evaluated by the gateway");
+		eventBasedGateway.setLocatorExpression("//*[local-name() = 'eventBasedGateway' and (@eventGatewayType = 'parallel')]");
+		eventBasedGateway.addAdaption("inclusiveGateway");
+		eventBasedGateway.addAdaption("complexGateway");
+		addToSet(eventBasedGateway);
 	}
 
 	private void buildEventBasedGateway() {
 		AdaptableElement eventBasedGateway = new AdaptableElement("eventBasedGateway");
 		eventBasedGateway.setDocumentation("An eventBasedGateway can be adapted to a solution where the events are processed through tasks before the gateway and set conditions to be evaluated by the gateway");
-		eventBasedGateway.setLocatorExpression("//*[local-name() = 'eventBasedGateway']");
+		eventBasedGateway.setLocatorExpression("//*[local-name() = 'eventBasedGateway' and not (@eventGatewayType = 'parallel')]");
 		eventBasedGateway.addAdaption("parallelEventBasedGateway");
 		eventBasedGateway.addAdaption("inclusiveGateway");
 		eventBasedGateway.addAdaption("complexGateway");
