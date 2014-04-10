dbd2ce0422e2942b3c5265d3b0867dd9deae7b99
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2014-04-10 10:00:03 +0200

    Add collection of elements that should only be detected

---
+++ src/main/java/pete/metrics/adaptability/elements/DetectionOnlyElements.java
@@ -0,0 +1,44 @@
+package pete.metrics.adaptability.elements;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+class DetectionOnlyElements {
+
+	private final Collection<AdaptableElement> elements;
+
+	public DetectionOnlyElements() {
+		elements = new HashSet<>();
+	}
+
+	private void addToSet(AdaptableElement element) {
+		boolean success = elements.add(element);
+		if (!success) {
+			throw new IllegalStateException(element.getName()
+					+ " was tried to be added twice");
+		}
+	}
+
+	public List<String> getElementNames() {
+		List<String> result = new ArrayList<>(elements.size());
+		elements.forEach(element -> result.add(element.getName()));
+		return result;
+	}
+
+	public List<AdaptableElement> getElements() {
+		List<AdaptableElement> result = new ArrayList<>(elements.size());
+		elements.forEach(element -> result.add(element));
+		return result;
+	}
+
+	public Map<String, AdaptableElement> getElementsByName() {
+		HashMap<String, AdaptableElement> result = new HashMap<>();
+		elements.forEach(element -> result.put(element.getName(), element));
+		return result;
+	}
+
+}
