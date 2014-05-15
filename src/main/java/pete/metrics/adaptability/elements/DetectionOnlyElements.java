e4f0f3bf4f5ac437a364c7a86e95e9715d71e55d
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2014-05-15 13:22:39 +0200

    Set task to detectionOnly

--- src/main/java/pete/metrics/adaptability/elements/DetectionOnlyElements.java
+++ src/main/java/pete/metrics/adaptability/elements/DetectionOnlyElements.java
@@ -1,6 +1,5 @@
 package pete.metrics.adaptability.elements;
 
-
 class DetectionOnlyElements extends ElementsCollection {
 
 	private EventElements events;
@@ -10,24 +9,37 @@ class DetectionOnlyElements extends ElementsCollection {
 		buildSequenceFlow();
 		buildThrowLinkEvent();
 		buildCatchLinkEvent();
+		buildTask();
 	}
 
 	private void buildSequenceFlow() {
-		AdaptableElement sequenceFlow = new AdaptableElement("sequenceFlow", true);
+		AdaptableElement sequenceFlow = new AdaptableElement("sequenceFlow",
+				true);
 		sequenceFlow.setLocatorExpression("//*[local-name() = 'sequenceFlow']");
 		add(sequenceFlow);
 	}
 
 	private void buildThrowLinkEvent() {
-		AdaptableElement linkEvent = new AdaptableElement("linkThrowEvent", true);
-		linkEvent.setLocatorExpression(events.buildEventXPathExpression("intermediateThrowEvent", "link"));
+		AdaptableElement linkEvent = new AdaptableElement("linkThrowEvent",
+				true);
+		linkEvent.setLocatorExpression(events.buildEventXPathExpression(
+				"intermediateThrowEvent", "link"));
 		add(linkEvent);
 	}
 
 	private void buildCatchLinkEvent() {
-		AdaptableElement linkEvent = new AdaptableElement("linkCatchEvent", true);
-		linkEvent.setLocatorExpression(events.buildEventXPathExpression("intermediateCatchEvent", "link"));
+		AdaptableElement linkEvent = new AdaptableElement("linkCatchEvent",
+				true);
+		linkEvent.setLocatorExpression(events.buildEventXPathExpression(
+				"intermediateCatchEvent", "link"));
 		add(linkEvent);
+	}
+
+	private void buildTask() {
+		AdaptableElement task = new AdaptableElement("task", true);
+		task.setLocatorExpression("//*[local-name() = 'task' or local-name() = 'globalTask']");
+		task.setDocumentation("A plain task is a kind of wildcard element for an unspecified task an irrelevant to process execution");
+		add(task);
 	}
 
 }
