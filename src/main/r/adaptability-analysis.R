ce0fff1a73c0902d5f0c600a859452d551d4b676
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2014-05-26 13:58:05 +0200

    Add R-script for evaluating adaptability metrics

--- src/main/r/adaptability-analysis.R
+++ src/main/r/adaptability-analysis.R
@@ -2,3 +2,11 @@ ds = read.csv("c:/workspaces/git/pete/r-results.csv")
 library(Rcmdr)
 
 prop.table(summary(ds$isCorrectNamespace))
+prop.table(summary(ds$referencesIssuesFound))
+prop.table(summary(ds$isExecutable))
+
+constructs = read.csv("c:/workspaces/git/pete/raw.csv")
+constructsSorted = constructs[order(constructs$number),]
+constructsRemoved = constructsSorted[(constructsSorted$number > 80),]
+par(mar=c(6, 12, 4, 2) + 0.1)
+barplot(constructsRemoved$number, horiz=TRUE,names.arg=constructsRemoved$element, las=1,cex.names=0.8)
