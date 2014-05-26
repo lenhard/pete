3845384d289d5ac79ce27f79c64754a39f6956c5
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2014-05-26 14:25:34 +0200

    Compute xlab names as percentages

--- src/main/r/adaptability-analysis.R
+++ src/main/r/adaptability-analysis.R
@@ -7,6 +7,6 @@ prop.table(summary(ds$isExecutable))
 
 constructs = read.csv("c:/workspaces/git/pete/raw.csv")
 constructsSorted = constructs[order(constructs$number),]
-constructsRemoved = constructsSorted[(constructsSorted$number > 80),]
+constructsRemoved = constructsSorted[(constructsSorted$number > 150),]
 par(mar=c(6, 12, 4, 2) + 0.1)
 barplot(constructsRemoved$number, horiz=TRUE,names.arg=constructsRemoved$element, las=1,cex.names=0.8)
