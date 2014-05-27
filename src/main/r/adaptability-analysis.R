cb2cfecc74dfc7555e2b0946a7008838cb47213f
Joerg Lenhard <joerg.lenhard@uni-bamberg.de>
2014-05-27 11:10:03 +0200

    src/main/r/adaptability-analysis.R
    Add commands for Shapiro-Wilk and Mann-Whitney U

--- src/main/r/adaptability-analysis.R
+++ src/main/r/adaptability-analysis.R
@@ -16,7 +16,7 @@ constructsRemoved$number <- (constructsRemoved$number / 2995) * 100
 par(mar=c(6, 12, 4, 2) + 0.1)
 barplot(constructsRemoved$number, horiz=TRUE,names.arg=constructsRemoved$element, las=1,cex.names=0.8)
 
-#group in exec, non-exec
+#group in executable and non-executable
 exec <- subset(ds, ds$isExecutable=='true')
 nonExec <- subset(ds, ds$isExecutable=='false')
 nrow(exec)
@@ -24,16 +24,46 @@ nrow(nonExec)
 
 # compute descriptive metric data
 summary(exec$AD0.2)
+sd(exec$AD0.2, na.rm=TRUE)
 summary(exec$AD0.4)
+sd(exec$AD0.4, na.rm=TRUE)
 summary(exec$AD0.6)
+sd(exec$AD0.6, na.rm=TRUE)
 summary(exec$AD0.8)
+sd(exec$AD0.8, na.rm=TRUE)
 summary(exec$wAD)
+sd(exec$wAD, na.rm=TRUE)
 
 summary(nonExec$AD0.2)
+sd(nonExec$AD0.2, na.rm=TRUE)
 summary(nonExec$AD0.4)
+sd(nonExec$AD0.4, na.rm=TRUE)
 summary(nonExec$AD0.6)
+sd(nonExec$AD0.6, na.rm=TRUE)
 summary(nonExec$AD0.8)
+sd(nonExec$AD0.8, na.rm=TRUE)
 summary(nonExec$wAD)
+sd(nonExec$wAD, na.rm=TRUE)
+
+# check if the sets are normally distributed (Shapiro-Wilk)
+shapiro.test(exec$AD0.2)
+shapiro.test(exec$AD0.4)
+shapiro.test(exec$AD0.6)
+shapiro.test(exec$AD0.8)
+shapiro.test(exec$wAD)
+
+shapiro.test(nonExec$AD0.2)
+shapiro.test(nonExec$AD0.4)
+shapiro.test(nonExec$AD0.6)
+shapiro.test(nonExec$AD0.8)
+shapiro.test(nonExec$wAD)
+
+# check for difference in the distributions (Mann-Whitney U)
+wilcox.test(exec$AD0.2, nonExec$AD0.2, alternative="greater")
+wilcox.test(exec$AD0.4, nonExec$AD0.4, alternative="greater")
+wilcox.test(exec$AD0.6, nonExec$AD0.6, alternative="greater")
+wilcox.test(exec$AD0.8, nonExec$AD0.8, alternative="greater")
+wilcox.test(exec$wAD, nonExec$wAD, alternative="greater")
 
 # compute amount of unique values
 unique(exec$AD0.2)
@@ -58,4 +88,4 @@ small <- subset(ds,ds$elements < 7)
 large <- subset(ds,ds$elements > 15)
 nrow(small)
 nrow(large)
-wilcox.test(small$wAD,large$wAD, paired=TRUE)
+wilcox.test(small$wAD,large$wAD, alternative="greater")
